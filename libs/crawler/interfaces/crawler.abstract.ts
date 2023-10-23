188c052ded5c0fece321806b919ae4eac4154332
jaehoon.ahn <jaehoon.ahn@sellerhub.co.kr>
2023-10-23 22:26:23 +0900

    feature: 네이버 부동산 크롤러 개발
    - Crawler Module 추가
    - NaverLandCrawler Module 추가
    - 테스트 코드 작성

---
+++ libs/crawler/interfaces/crawler.abstract.ts
@@ -0,0 +1,17 @@
+import {
+    CrawlerClient,
+    CrawlerDto,
+    CrawlerParseResponse,
+    CrawlerType,
+} from '@libs/crawler/interfaces/crawler.interface';
+
+export abstract class CrawlerAbstract<T extends CrawlerType> {
+    protected constructor(
+        protected readonly client: CrawlerClient<T>,
+        protected readonly config?: any,
+    ) {}
+
+    abstract run(dto: CrawlerDto<T>): Promise<any>;
+
+    abstract parse(data: CrawlerParseResponse<T>): any;
+}
