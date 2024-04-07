d7e09a8238a08aea9b3ec921ff45e6313e1d1df6
jissp <jissps@naver.com>
2024-04-07 09:43:31 +0900

    feature: database config에 timezone 설정

--- libs/config/services/config.service.ts
+++ libs/config/services/config.service.ts
@@ -1,17 +1,38 @@
 import { IConfig } from '../interfaces/config.interface';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import * as process from 'process';
 import { BullRootModuleOptions } from '@nestjs/bull/dist/interfaces/bull-module-options.interface';
 
 export class ConfigService implements IConfig {
     async getDatabaseConfig(): Promise<TypeOrmModuleOptions> {
-        return Promise.resolve(undefined);
+        return {
+            type: 'mysql',
+            host: process.env['database_host'],
+            port: Number(process.env['database_port']),
+            database: process.env['database_collection'],
+            username: process.env['database_user'],
+            password: process.env['database_password'],
+            timezone: 'Asia/Seoul',
+            synchronize: false,
+            autoLoadEntities: true,
+            extra: {
+                decimalNumbers: true,
+            },
+            namingStrategy: new SnakeNamingStrategy(),
+        };
     }
 
     async getKakaoLocalApiKey(): Promise<string> {
-        return Promise.resolve('');
+        return process.env['kakao_rest_api_key'];
     }
 
     async getRedisConfig(): Promise<BullRootModuleOptions> {
-        return {};
+        return {
+            redis: {
+                host: process.env['redis_host'],
+                port: Number(process.env['redis_port']),
+            },
+        };
     }
 }
